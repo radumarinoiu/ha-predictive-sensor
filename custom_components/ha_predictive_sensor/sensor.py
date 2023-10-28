84b95ddbb8e4c4e438321a0aa98ef4d2cf9951d6
Radu Florin Marinoiu <radumarinoiu98@gmail.com>
2023-10-28 16:05:42 +0300

    Version 0.0.7

--- custom_components/ha_predictive_sensor/sensor.py
+++ custom_components/ha_predictive_sensor/sensor.py
@@ -1,13 +1,16 @@
+import datetime
 import logging
 
+from homeassistant.components.recorder import get_instance, history
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.const import STATE_UNAVAILABLE, STATE_UNKNOWN, EVENT_HOMEASSISTANT_START, CONF_NAME, CONF_UNIQUE_ID
-from homeassistant.core import callback, CoreState, HomeAssistant
+from homeassistant.core import callback, CoreState, HomeAssistant, State
 from homeassistant.helpers.typing import ConfigType
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.event import async_track_state_change_event
 from homeassistant.helpers.reload import async_setup_reload_service
 from homeassistant.helpers.restore_state import RestoreEntity
+from homeassistant.util import dt as dt_util
 
 from custom_components.ha_predictive_sensor import PLATFORMS, DOMAIN
 from custom_components.ha_predictive_sensor.const import CONF_SENSOR
@@ -61,27 +64,48 @@ class PredictiveSensor(SensorEntity, RestoreEntity):
         return self._predicted_temp
 
     def _recalculate_prediction(self):
-        while len(self._sensor_temperature_history) > self.max_history_entries:
-            self._sensor_temperature_history.pop(0)
+        """This currently does average, but it should do prediction in the future"""
+        # while len(self._sensor_temperature_history) > self.max_history_entries:
+        #     self._sensor_temperature_history.pop(0)
         if self._sensor_temperature_history:
             self._predicted_temp = sum(self._sensor_temperature_history)/len(self._sensor_temperature_history)
+
+    async def _update_sensor_history(self):
+        end = datetime.datetime.now()
+        start = end - datetime.timedelta(hours=2)
+        start, end = dt_util.as_utc(start), dt_util.as_utc(end)
+        _sensor_temperature_histories = await get_instance(self.hass).async_add_executor_job(
+            history.state_changes_during_period,
+            self.hass,
+            start,
+            end,
+            self.temperature_entity_id,
+        )
+        self._sensor_temperature_history = _sensor_temperature_histories.get(self.temperature_entity_id, list())
+        _LOGGER.debug(self._sensor_temperature_history)
 
     @callback
     def _async_update_temp(self, state):
         """Update thermostat with latest state from sensor."""
         try:
-            self._sensor_temperature_history.append(float(state.state))
+            # self._sensor_temperature_history.append(float(state.state))
             self._recalculate_prediction()
         except ValueError as ex:
             _LOGGER.error("Unable to update from sensor: %s", ex)
 
-
     async def _async_sensor_changed(self, event):
         """Handle temperature changes"""
-        new_state = event.data.get("new_state")
+        new_state: State = event.data.get("new_state")
         _LOGGER.info(f"Sensor change: {event.data} -> {new_state}")
         if new_state is None or new_state.state in (STATE_UNAVAILABLE, STATE_UNKNOWN):
             return
+
+        if new_state.entity_id != self.temperature_entity_id:
+            return
+
+        await self._update_sensor_history()
+        await self._async_update_temp(new_state)
+
 
     async def async_added_to_hass(self):
         """Run when entity about to be added."""
